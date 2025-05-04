01e7035c0a05e8e5203b2d5686e5b5e0496b95aa
Qois <qois.firosi@gmail.com>
2025-05-04 22:47:09 +0700

    Pokoknya yg penting login jadi dulu dah
    Fix home
    
    Seadanya dulu utk login
    
    Login jadi dulu dah

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const authRoutes = require('./auth/routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/admin', (req, res) => {
+  console.log('Serving login.html');
+  res.sendFile(path.join(__dirname, 'public', 'loginFrontend.html'));
+});
+
+// Static files middleware should come after the route
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Auth routes
+app.use('/auth', authRoutes);
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
